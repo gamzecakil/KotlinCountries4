f0f2ce553025794647f8233348d40a1017ec5a20
gamzecakil <gmzcakil@gmail.com>
2024-03-07 15:29:46 +0300

    apiden çektiğimiz verileri alabilmemiz ve gösterebilmemiz için gerekli olan model mizi attributelarıyla yazdık.

---
+++ app/src/main/java/com/gamzeuysal/kotlincountries4/model/Model.kt
@@ -0,0 +1,10 @@
+package com.gamzeuysal.kotlincountries4.model
+
+data class Country(val countryName :String?,
+        val countryRegion:String?,
+        val countryCapital:String?,
+        val countyrCurrency :String?,
+        val countryLanguage:String?,
+        val imageUrl:String? )
+{
+}
